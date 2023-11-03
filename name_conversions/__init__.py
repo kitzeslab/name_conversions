e72d2e1134224e4005a4f5042d00b38c6738f29f
sammlapp <sammlapp@gmail.com>
2023-11-03 00:51:44 -0400

    add alpha code translation with IBP and BBL options. Copied tables from the GoldenEye repo.
    Also added all functions into the top-level package namespace

--- name_conversions/__init__.py
+++ name_conversions/__init__.py
@@ -1,2 +1,3 @@
 from . import name_conversions
 from . import resources
+from name_conversions.name_conversions import *
--- name_conversions/name_conversions.py
+++ name_conversions/name_conversions.py
@@ -8,6 +8,16 @@ import pandas as pd
 with importlib.resources.path("name_conversions.resources", "species_table.csv") as f:
     species_table = pd.read_csv(f)
 
+with importlib.resources.path(
+    "name_conversions.resources", "bbl-alpha-codes_2021.csv"
+) as f:
+    bbl_alpha = pd.read_csv(f)
+
+with importlib.resources.path(
+    "name_conversions.resources", "ibp-alpha-codes_2021.csv"
+) as f:
+    ibp_alpha = pd.read_csv(f)
+
 
 def get_species_list():
     """returns a list of scientific-names (lowercase-hyphenated) of species"""
@@ -23,6 +33,12 @@ def get_species_list():
 name_table_sci_idx = species_table.set_index("scientific", drop=True)
 name_table_xc_com_idx = species_table.set_index("xc_common", drop=True)
 name_table_bn_com_idx = species_table.set_index("bn_common", drop=True)
+ibp_alpha_alpha_idx = ibp_alpha.set_index("true_alpha", drop=True)
+bbl_alpha_alpha_idx = bbl_alpha.set_index("true_alpha", drop=True)
+ibp_alpha_sci_idx = ibp_alpha.set_index("scientific_name", drop=True)
+bbl_alpha_sci_idx = bbl_alpha.set_index("scientific_name", drop=True)
+ibp_alpha_com_idx = ibp_alpha.set_index("common_name", drop=True)
+bbl_alpha_com_idx = bbl_alpha.set_index("common_name", drop=True)
 
 
 def sci_to_bn_common(scientific):
@@ -53,3 +69,72 @@ def common_to_sci(common):
     Ignores dashes, spaces, case
     """
     return bn_common_to_sci(common)
+
+
+def alpha_to_common(alpha, alpha_type="ibp"):
+    """convert alpha code to common name; use BBL or IBP alpha codes
+
+    Args:
+        alpha: 4-letter alpha code (will be converted to uppercase)
+        alpha_type: 'bbl' or 'ibp'
+    """
+    assert isinstance(alpha, str) and len(alpha) == 4, "alpha must be a 4 letter string"
+    if alpha_type == "bbl":
+        return bbl_alpha_alpha_idx.at[alpha.upper(), "common_name"]
+    elif alpha_type == "ibp":
+        return ibp_alpha_alpha_idx.at[alpha.upper(), "common_name"]
+    else:
+        raise ValueError("alpha_type must be 'bbl' or 'ibp'")
+
+
+def alpha_to_sci(alpha, alpha_type="ibp"):
+    """convert alpha code to scientific name; use BBL or IBP alpha codes
+
+    Args:
+        alpha: 4-letter alpha code (will be converted to uppercase)
+        alpha_type: 'bbl' or 'ibp'
+    """
+    assert isinstance(alpha, str) and len(alpha) == 4, "alpha must be a 4 letter string"
+    if alpha_type == "bbl":
+        return bbl_alpha_alpha_idx.at[alpha.upper(), "scientific_name"]
+    elif alpha_type == "ibp":
+        return ibp_alpha_alpha_idx.at[alpha.upper(), "scientific_name"]
+    else:
+        raise ValueError("alpha_type must be 'bbl' or 'ibp'")
+
+
+def common_to_alpha(common, alpha_type="ibp"):
+    """convert common name to alpha code; use BBL or IBP alpha codes
+
+    Common name must match case and syntax exactly, for instance "Red-necked Grebe"
+
+    Args:
+        common: common name
+        alpha_type: 'bbl' or 'ibp'
+    """
+    if alpha_type == "bbl":
+        return bbl_alpha_com_idx.at[common, "true_alpha"]
+    elif alpha_type == "ibp":
+        return ibp_alpha_com_idx.at[common, "true_alpha"]
+    else:
+        raise ValueError("alpha_type must be 'bbl' or 'ibp'")
+
+
+def sci_to_alpha(sci, alpha_type="ibp"):
+    """convert scientific name to alpha code; use BBL or IBP alpha codes
+
+    Args:
+        sci: scientific name
+            Note: converts "-" to space, first letter to uppercase, and rest to lowercase
+            This assists in matching the format "Genus species" eg "Crux crux"
+        alpha_type: 'bbl' or 'ibp'
+    """
+    sci = sci.replace("-", " ")
+    sci = sci[0].upper() + sci[1:].lower()
+
+    if alpha_type == "bbl":
+        return bbl_alpha_sci_idx.at[sci, "true_alpha"]
+    elif alpha_type == "ibp":
+        return ibp_alpha_sci_idx.at[sci, "true_alpha"]
+    else:
+        raise ValueError("alpha_type must be 'bbl' or 'ibp'")
